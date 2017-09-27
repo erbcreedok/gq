4f5bcdd976e1a0999c1e8714217d3a4ab67ff759
erbcreedok <admin@MacBookPro.local>
2017-09-27 20:18:59 +0600

    added sub image to catalog

--- kazprom_outlet_invis/assets/js/itemload.script.js
+++ kazprom_outlet_invis/assets/js/itemload.script.js
@@ -9,6 +9,7 @@ var config = {
 firebase.initializeApp(config);
 var base = firebase.database();
 var activeImage = 0;
+var activeSection = 0;
 var images = [];
 var displayValues = ['none', 'block'];
 function loadData() {
@@ -22,16 +23,24 @@ function loadData() {
         if (data) {
             $(".title-here").html(data.title);
             var rawData = '';
-            rawData += '<div class="item-titles"><a href="catalog.html' + selectedUrl + '" class="lightblue"> <i class="glyphicon glyphicon-chevron-left"></i> Назад к списку </a><h5 class="item-title">' + data.title + '</h5></div><div class="catalog-item"><div class="catalog-item-image item-only" style="background-image: url(' + data.images[0] + ')"><img src="assets/img/full.png" onmouseup="toggleClass(\'popup\',\'viewed\')" height="34px"></div><div class="item-text"><h5>' + data.description[0].title + '</h5><br><p class="classic small">' + data.description[0].value + '</p><br></div>';
+            rawData += '<div class="item-titles"><a href="catalog.html' + selectedUrl + '" class="lightblue"> <i class="glyphicon glyphicon-chevron-left"></i> Назад к списку </a><h5 class="item-title">' + data.title + '</h5></div><div class="catalog-item"><div class="catalog-item-image item-only" id="image-here-bg" style="background-image: url(' + data.images[0] + ')"><div onclick="onPopupImageClicked(1)" class="image-changer"></div><img src="assets/img/full.png" onmouseup="toggleClass(\'popup\',\'viewed\')" height="34px"></div><div class="catalog-item-images"><div class="catalog-item-images-container">';
+            
+            for (var i = 0; i < images.length; i++) {
+                rawData += '<div class="mini-image" onclick="onMiniImageClicked(' + i + ')" style="background-image: url(' + images[i] + ')"></div>';
+            }
+            
+            rawData += '</div><img onclick="onImageSectionChanged(-1)" class="catalog-item-navigation left-navigation" src="assets/img/left_black.png"><img onclick="onImageSectionChanged(1)" class="catalog-item-navigation right-navigation" src="assets/img/right_black.png"></div><div class="item-text"><h5>' + data.description[0].title + '</h5><br><p class="classic small">' + data.description[0].value + '</p><br></div>';
+            
             for (var i = 1; i < data.description.length; i++) {
                 rawData += '<h5>' + data.description[i].title + '</h5><p class="classic small">' + data.description[i].value + '</p><br>';
             }
+            
             rawData += '<span class="catalog-item-date"></span></div>';
             $("#catalog-item").html(rawData);
             $("#catalog-also-viewed").css("display","block");
             rawData = '<div class="circle"></div>'.repeat(images.length);
             $("popup .circles").html(rawData);
-            onPopupImageChanged();
+            onImageChanged();
         } else {
             $(".title-here").html("Товар не найден");
             var rawData = '';
@@ -47,13 +56,38 @@ function onPopupImageClicked(value) {
         activeImage = images.length - 1;
     }
     activeImage %= images.length;
+    onImageChanged();
+}
+
+function onMiniImageClicked(value) {
+    activeImage = value;
+    onImageChanged();
+}
+
+function onImageChanged() {
     onPopupImageChanged();
+    onMiniImageChanged();
 }
 
 function onPopupImageChanged() {
-    $("#image-here").attr("src", images[activeImage]);
-    $("popup .circles .circle").removeClass("active");
-    $("popup .circles .circle:eq(" + activeImage + ")").addClass("active");
-    $("popup .toleft").css('display', displayValues[0 + (activeImage !== 0)]);
-    $("popup .toright").css('display', displayValues[0 + (activeImage !== images.length - 1 )]);
+    $('#image-here').attr('src', images[activeImage]);
+    $('popup .circles .circle').removeClass('active');
+    $('popup .circles .circle:eq(' + activeImage + ')').addClass('active');
+    $('popup .toleft').css('display', displayValues[0 + (activeImage !== 0)]);
+    $('popup .toright').css('display', displayValues[0 + (activeImage !== images.length - 1 )]);
+}
+
+function onMiniImageChanged() {
+    $('#image-here-bg').attr('style', 'background-image: url(' + images[activeImage] + ')');
+    $('.mini-image').removeClass('active');
+    $('.mini-image:eq(' + activeImage + ')').addClass('active');
+    sectionIndex = Math.floor(activeImage/4);
+    onImageSectionChanged(0);
+}
+
+function onImageSectionChanged(value) {
+    sectionIndex += value;
+    $('.catalog-item-navigation.left-navigation').css('display', displayValues[0 + (sectionIndex !== 0)]);
+    $('.catalog-item-navigation.right-navigation').css('display', displayValues[0 + (sectionIndex !== Math.floor(images.length/4))]);  
+    $('.catalog-item-images-container').css('margin-top', -60 * (sectionIndex )); 
 }
