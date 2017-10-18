ca25ae52ec329fc4fd15c874de634bd1a6462b2e
Yerbol Syzdyk <erbcreedok@gmail.com>
2017-10-18 07:58:24 +0600

    added "catalog" page

--- home-style/assets/js/index.animation.js
+++ home-style/assets/js/index.animation.js
@@ -21,11 +21,9 @@ function switchDoor() {
     $('.doorsLengthHere').html(doors.length);
 }
 function onDoorPressed(num) {
-    console.log(num, activeDoor, doors.length);
     activeDoor = (activeDoor + num + doors.length) % doors.length;
-    console.log(num, activeDoor, doors.length);
     var rotate = (num === 1) ? 'left' : 'right';
-    var elem = $(".door-slider");
+    var elem = $('.door-slider');
     console.log(rotate);
     $(elem).removeClass('door-slider-in-left door-slider-in-right').addClass('door-slider-out-' + rotate);
     doorTimer = setTimeout(function() {
@@ -34,3 +32,58 @@ function onDoorPressed(num) {
         $(elem).removeClass('door-slider-out-left door-slider-out-right').addClass('door-slider-in-' + rotate);
     }, 150);
 }
+
+var workers = [
+    {
+        information: '<h2>Наш директор</h2><p class="cursive gray">Принцип нашей работы</p><p class="gray">Так же можно получить как дополнение к самой двери на выбор или заказать отдельно к же можно получить как дополнение к самой двери на выбор или заказать отдельно олнение к самой двери на выбор или заказать отдельно</p> <img src="assets/images/handwrite.png" style="margin: -10px 0px 0px 10px" width="125px">',
+        src: 'assets/images/director.jpg'
+    },
+    {
+        information: '<h2>Мираш Байжуманова</h2><p class="cursive gray">Принцип нашей работы</p><p class="gray">Так же можно получить как дополнение к самой двери на выбор или заказать отдельно к же можно получить как дополнение к самой двери на выбор или заказать отдельно олнение к самой двери на выбор или заказать отдельно</p> <img src="assets/images/handwrite.png" style="margin: -10px 0px 0px 10px" width="125px">',
+        src: 'assets/images/team2.jpg'
+    },
+    {
+        information: '<h2>Исполнительный директор</h2><p class="cursive gray">Принцип нашей работы</p><p class="gray">Так же можно получить как дополнение к самой двери на выбор или заказать отдельно к же можно получить как дополнение к самой двери на выбор или заказать отдельно олнение к самой двери на выбор или заказать отдельно</p> <img src="assets/images/handwrite.png" style="margin: -10px 0px 0px 10px" width="125px">',
+        src: 'assets/images/team3.jpg'
+    }
+];
+var activeWorker = 0;
+var workerTimer;
+function switchWorker() {
+    $('.workerInformationHere').html(workers[activeWorker].information);
+    $('.workerImageHere').css({'background-image' : 'url(' + workers[activeWorker].src + ')'});
+    var circles = $('.workerCircles');
+    $(circles).html('');
+    for (var i = 0; i < workers.length; i++) {
+        $(circles).append('<i class="fa fa-circle" onclick="animateWorker(' + i + ')"></i>');
+    }
+    $('.workerCircles i').eq(activeWorker).addClass('active');
+}
+function animateWorker(num) {
+    var rotate = '';
+    if (typeof num === 'number') {
+        num = (num + workers.length) % workers.length;
+        rotate = (num > activeWorker ) ? 'left' : 'right';
+        activeWorker = num;
+    } else {
+        rotate = num;
+        activeWorker = (activeWorker + (num === 'left' ? 1 : -1) + workers.length) % workers.length;
+    }
+    var elem = $('.worker-slider');
+    $(elem).removeClass('door-slider-in-left door-slider-in-right').addClass('door-slider-out-' + rotate);
+    workerTimer = setTimeout(function() {
+        clearTimeout(workerTimer);
+        switchWorker();
+        $(elem).removeClass('door-slider-out-left door-slider-out-right').addClass('door-slider-in-' + rotate);
+    }, 150);
+}
+$(document).on('click', '.workerArrowLeft, .workerArrowRight', function(){
+    animateWorker($(this).hasClass('workerArrowLeft') ? 'right' : 'left');
+});
+$(document).on('swipeleft', '.worker-slider-container', function(event) {
+    animateWorker('left');
+});
+$(document).on('swiperight', '.worker-slider-container', function(event) {
+    animateWorker('right');
+});
+switchWorker();
