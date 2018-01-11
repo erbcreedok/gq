22b418b3d85957813d393c585f7201c8a420dcba
Yerbol Syzdyk <erbcreedok@gmail.com>
2018-01-11 21:36:19 +0600

    sercons main page 90% completed

--- sercons/js/main.js
+++ sercons/js/main.js
@@ -25,3 +25,19 @@
 
     countersAnimate();
 })();
+
+function formSubmitted(event) {
+    console.log(event);
+    return false;
+}
+
+$(document).on('submit','form',function(event) {
+    var name = event.target[1].value;
+    var phone = event.target[2].value;
+    var message = name + ", " + phone;
+    console.log(message);
+    $.get( "https://api.telegram.org/bot536740514:AAFCPBUfgoabiFO-alU2Dz_WGiPWqmoKehc/sendMessage?chat_id=-183675593 &parse_mode=html&text="+message, function() {
+        alert("Ваша заявка принята, мы перезвоним вам в ближайшее время");
+    });
+    return false;
+});
